d4283843da7ca615a6805f67abef7a06e2c7106d
Essra-Hmida <hmidaesraa@gmail.com>
2024-04-14 20:09:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/essra_hmida_mesure_glycemie/controller/Controller.java
@@ -0,0 +1,32 @@
+package com.example.essra_hmida_mesure_glycemie.controller;
+
+import com.example.essra_hmida_mesure_glycemie.model.Patient;
+
+public class Controller {
+    private static Patient patient;
+    private static Controller instance=null;
+
+    private Controller() {
+        super();
+    }
+    public static final Controller getInstance()
+    {
+        if (instance == null)
+            instance=new Controller();
+        return instance;
+    }
+
+    // fleche "userAction" view-->controller --> model
+    public void createPatient(int age, float valeurMesuree, boolean isFasting)
+    {
+        patient =new Patient(age,valeurMesuree,isFasting);
+    }
+
+    // fleche "notify" controller --> view
+    public String getResult()
+    {
+        return patient.getResult();
+    }
+
+}
+
